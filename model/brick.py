a7bc88b82b8ed3c2da1f1d8dea11314e41181b2c
Martin White <MartinJamesWhite@gmail.com>
2015-03-06 08:46:05 -0800

    Added first pass at documentation and heritage information.

--- model/brick.py
+++ model/brick.py
@@ -1,23 +1,34 @@
+# The base Python object corresponding to an imaging brick.
+# This object contains only the meta-data, and it calls
+# imagerepo to handle looking up the pixel-level information.
 
 import numpy
 import wcs_tangent
 
-class Brick(object):
-    def __init__(self, id, name, ra, dec, ra1, ra2, dec1, dec2):
-        """ initialize a brick object 
-            Brick is immutable .
+__author__ = "Yu Feng and Martin White"
+__version__ = "1.0"
+__email__  = "yfeng1@berkeley.edu or mjwhite@lbl.gov"
 
-            wcs describes the coordinate tranformation from 
-            RA DEC to xy. 
-            def wcs(ra, dec, xout, yout):
-                ...
-                xout[:], yout[:] = .....  """
-        self.id = id
+
+
+
+class Brick(object):
+    """
+    The base (immutable) object corresponding to an imaging brick.
+    This object contains only the meta-data, and it calls
+    imagerepo to handle looking up the pixel-level information.
+    Major methods are "readout", "query" and "revert".
+    """
+    def __init__(self, id, name, ra, dec, ra1, ra2, dec1, dec2):
+        """
+        Initialize a brick object.  Brick is immutable.
+        """
+        self.id   = id
         self.name = name
-        self.ra = ra
-        self.dec = dec
-        self.ra1 = ra1
-        self.ra2 = ra2
+        self.ra   = ra
+        self.dec  = dec
+        self.ra1  = ra1
+        self.ra2  = ra2
         self.dec1 = dec1
         self.dec2 = dec2
         #print elf.query(([self.ra], [self.dec]))
@@ -27,43 +38,38 @@ class Brick(object):
         return self.id
 
     def __repr__(self):
-        return ("Brick(id=%d, name=%s, ra=%g, dec=%g, ...)"
+        return ("Brick(id=%d, name=%s, ra=%g, dec=%g, ...)"\
             % (self.id, self.name, self.ra, self.dec ))
 
     def readout(self, coord, repo, default=numpy.nan):
-        """ readout image values at coord = (RA, DEC) 
-
-            if the image does not cover this region, put in `default'.
-
-            returns the image value at coord.            
         """
-        meta = repo.metadata(self)
-        img = repo.open(self)
-        
-        RA, DEC = coord
-        value = numpy.empty(len(RA))
-        value[...] = default
-
-        coord = numpy.array(coord)
+        Return image values at coord=(RA, DEC) using the imagerepository repo.
+        If the image in this brick does not cover this region,
+        put in `default'.
+        """
+        meta      = repo.metadata(self)
+        img       = repo.open(self)
+        RA, DEC   = coord
+        value     = numpy.empty(len(RA))
+        value[...]= default
+        coord     = numpy.array(coord)
         xy = numpy.int32(wcs_tangent.ang2pix_hdr(coord, meta, 
                 zero_offset=True))
         xy2 = numpy.int32(self.query(coord))
-
         mask = (xy < numpy.array(img.shape) \
             .reshape(2, 1)).all(axis=0)
-
         mask &= (xy >= 0).all(axis=0)
-
         l = numpy.ravel_multi_index(xy[:, mask], img.shape, mode='raise')
         value[mask] = img.flat[l] 
         return value
  
     def query(self, coord):
-        """ returns the xy index of pixels for coord
-            coord can be:
-                (RA, DEC) tuple of arrays
-                array of shape (2xN) RA DEC
-            returns (2xN)
+        """
+        Returns the xy index of pixels at coord
+        coord can be:
+            (RA, DEC) tuple of arrays
+             array of shape (2xN) RA DEC
+        In either case returns a (2xN) array
         """
         #FIXME: other types of input
         coord = numpy.array(coord)
@@ -75,11 +81,12 @@ class Brick(object):
         return out
 
     def revert(self, xy):
-        """ returns the RA, DEC index of pixels for coord
-            coord can be:
-                (x, y) tuple of arrays
-                array of shape (2xN) x, y
-            returns (2xN)
+        """
+        Returns the RA, DEC index of pixels for coord
+        coord can be:
+            (x, y) tuple of arrays
+            array of shape (2xN) x, y
+        In either case returns a (2xN) array.
         """
         #FIXME: other types of input
         xy = numpy.array(xy)
