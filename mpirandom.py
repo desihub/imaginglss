c273775489eca28f762a77f08df989e431ed8da0
Yu Feng <yfeng1@berkeley.edu>
2015-03-09 22:47:38 -0700

    document usage of mpirandom.py
    add milkyway ebv term to the end of the output.
    add second argument (output filename)

--- mpirandom.py
+++ mpirandom.py
@@ -1,10 +1,36 @@
+"""
+sample random points with in the RA DEC range of the survy
+
+usage:
+set DECALS_IMAGING and DECALS_CACHE
+ensure the milky way extinction bricks are generated (build_sdfbricks.py)
+
+On Edison, in a job, run
+
+  aprun -n 24 python-mpi mpirandom.py 400000 random.txt
+
+  400000 is the number of random points 
+  random.txt is the output.
+
+on front end, run
+
+  python mpirandom.py 400000 random.txt
+
+  (will be a lot slower than with MPI)
+
+  output format is added at the top of file as a comment
+
+ Yu Feng <yfeng1@berkeley.edu>
+ Martin White <mwhite@berkeley.edu>
+""" 
 from model.datarelease import DataRelease
 import numpy
 import sys
-from sys import stdout
 from sys import argv
+
 def main(comm):
     N = int(argv[1])
+    output = argv[2]
     dr = DataRelease()
     bi = dr.brickindex
 
@@ -34,16 +60,20 @@ def main(comm):
     mydepth[:, 1] = dr.readout(coord, dr.images['depth']['r'])
     mydepth[:, 2] = dr.readout(coord, dr.images['depth']['g'])
     mydepth[:, 4] = dr.readout(coord, dr.images['depth']['z'])
+    myebv = numpy.zeros(len(RA[sl]), dtype='f4')
+    myebv[:] = dr.readout(coord, dr.images['ebv'])
     if comm is not None:
         depth = comm.gather(mydepth)
+        ebv = comm.gather(myebv)
         if comm.rank == 0:
             depth = numpy.concatenate(depth, axis=0)
+            ebv = numpy.concatenate(ebv, axis=0)
         else:
             return
     else:
         depth = mydepth
-
-    numpy.savetxt(stdout, zip(RA, DEC, depth[:, 1], depth[:, 2], depth[:, 4]), header='#ra dec r g z')
+        ebv = myebv
+    numpy.savetxt(output, zip(RA, DEC, depth[:, 1], depth[:, 2], depth[:, 4], ebv), fmt='%.6f', header='#ra dec r g z ebv')
 
 if __name__ == '__main__':    
     from sys import argv
