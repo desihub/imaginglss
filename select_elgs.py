ad6bff838afa28022473df41b2a2438af14bd29e
Martin White <mjwhite@lbl.gov>
2015-03-12 14:04:31 -0700

    Changed my E-mail.

--- select_elgs.py
+++ select_elgs.py
@@ -11,7 +11,7 @@ from __future__ import print_function
 
 __author__ = "Yu Feng and Martin White"
 __version__ = "1.0"
-__email__  = "yfeng1@berkeley.edu or mwhite@berkeley.edu"
+__email__  = "yfeng1@berkeley.edu or mjwhite@lbl.gov"
 
 
 
