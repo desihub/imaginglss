1e0183ee67bad401ece57174c223e5b1e6a4e4aa
Martin White <mjwhite@lbl.gov>
2015-03-12 14:09:41 -0700

    Changed E-mail.

--- footprint_mangle.py
+++ footprint_mangle.py
@@ -11,7 +11,7 @@ from __future__ import print_function
 
 __author__ = "Yu Feng and Martin White"
 __version__ = "1.0"
-__email__  = "yfeng1@berkeley.edu or mwhite@berkeley.edu"
+__email__  = "yfeng1@berkeley.edu and mjwhite@lbl.gov"
 
 import math  as M
 from model.datarelease import DataRelease
