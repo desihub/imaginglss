cdac655458d5e8fc59cfa2879325d60181dad20e
Yu Feng <yfeng1@berkeley.edu>
2015-04-09 16:14:28 -0700

    move EDR, DR to schema.py
    add docstrings.

--- model/datarelease.py
+++ model/datarelease.py
@@ -20,7 +20,7 @@ from utils import fits
 from . import brickindex
 from . import imagerepo
 from . import catalogue
-
+from . import schema
 
 __author__ = "Yu Feng and Martin White"
 __version__ = "0.9"
@@ -72,84 +72,6 @@ def contains(haystack, needle):
     ind.clip(0, len(haystack) - 1, ind)
     return haystack[ind] == needle
 
-class EDR:
-    BRICKS_FILENAME = 'bricks.fits'
-    CATALOGUE_ALIASES = [('EXTINCTION', 'DECAM_MW_TRANSMISSION', lambda x: 10**(x/-2.5))]
-
-    @staticmethod
-    def format_image_filenames():
-        images = {'depth': 'coadd/depth-%(brickid)d-%(band)s.fits.gz',
-         'image': 'coadd/image-%(brickid)d-%(band)s.fits',
-         'model': 'coadd/model-%(brickid)d-%(band)s.fits'}
-        imagerepos= {}
-        for image in images:
-            imagerepos[image] = {}
-            for band in 'rgz':
-                PATTERN = images[image]
-                def getfilename(brick, PATTERN=PATTERN, band=band):
-                    return PATTERN % dict(brickid=brick.id, band=band)
-                imagerepos[image][band] = getfilename
-        return imagerepos
-
-    @staticmethod
-    def format_catalogue_filename(brick):
-        TRACTOR_FILENAME = 'tractor/tractor-%(brickid)d.fits'
-        return TRACTOR_FILENAME % dict(brickid=brick.id) 
-
-    @staticmethod
-    def parse_filename(filename, brickindex):
-        if not filename.endswith('.fits'): raise ValueError
-        return brickindex.search_by_id(
-                int(re.search('-([0123456789]+)\.', 
-                os.path.basename(filename)).group(1)))
-
-class EDR3:
-    BRICKS_FILENAME = 'decals-bricks.fits'
-    CATALOGUE_ALIASES = [('DECAM_EXTINCTION', 'DECAM_MW_TRANSMISSION', lambda x: 10**(x/-2.5))]
-    
-    @staticmethod
-    def format_image_filenames():
-        images = {
-        'depth': 'coadd/%(pre)s/%(brickname)s/decals-%(brickname)s-depth-%(band)s.fits',
-        'model': 'coadd/%(pre)s/%(brickname)s/decals-%(brickname)s-model-%(band)s.fits',
-        'image': 'coadd/%(pre)s/%(brickname)s/decals-%(brickname)s-image-%(band)s.fits',
-        }
-        imagerepos= {}
-        for image in images:
-            imagerepos[image] = {}
-            for band in 'rgz':
-                PATTERN = images[image]
-                def getfilename(brick, PATTERN=PATTERN, band=band):
-                    return PATTERN % dict(pre=brick.name[:3], brickname=brick.name, band=band)
-                imagerepos[image][band] = getfilename
-
-        return imagerepos
-
-    @staticmethod
-    def format_catalogue_filename(brick):
-        TRACTOR_FILENAME = 'tractor/%(pre)s/tractor-%(brickname)s.fits'
-        return TRACTOR_FILENAME % dict(pre=brick.name[:3], brickname=brick.name) 
-    @staticmethod
-    def parse_filename(filename, brickindex):
-        if not filename.endswith('.fits'): raise ValueError
-        brickname = re.search('-([p0123456789]+)\.', 
-                os.path.basename(filename)).group(1)
-        bid = brickindex.search_by_name(brickname)
-        return bid
-
-class EDR4(EDR3):
-    CATALOGUE_ALIASES = []
-    pass
-
-class DR1(EDR4):
-    pass
-
-_configurations = {
-    'EDR': EDR,
-    'EDR3': EDR3,
-    'EDR4': EDR4,
-    'DR1': DR1,
-}
 class Footprint(object):
     """ footprint of a data release.
         
@@ -239,8 +161,10 @@ class DataRelease(object):
         if version is None:
             version = os.path.basename(root).upper()
 
-        config = _configurations[version]
+        if not hasattr(schema, version):
+            raise KeyError("Data Release of version %s is not supported" % version)
 
+        config = getattr(schema, version)
 
         self.cacheroot = os.path.join(cacheroot, version)
 
---
+++ model/schema.py
@@ -0,0 +1,98 @@
+""" The schema of DataReleases.
+
+    DataRelease Objects manages the migrating schema of EDR, DR. 
+    For example, if a field is renamed in DR from EDR, we want to modify
+    the EDR schema such that the code using DataRelease does not have to
+    be modified.
+
+    The `version` parameter of `DataRelease.__init__` picks the correct
+    schema. 
+"""
+class Schema:
+    pass
+
+class EDR(Schema):
+    """ Schema for EDR.
+
+    """
+    BRICKS_FILENAME = 'bricks.fits'
+    CATALOGUE_ALIASES = [('EXTINCTION', 'DECAM_MW_TRANSMISSION', lambda x: 10**(x/-2.5))]
+
+    @staticmethod
+    def format_image_filenames():
+        images = {'depth': 'coadd/depth-%(brickid)d-%(band)s.fits.gz',
+         'image': 'coadd/image-%(brickid)d-%(band)s.fits',
+         'model': 'coadd/model-%(brickid)d-%(band)s.fits'}
+        imagerepos= {}
+        for image in images:
+            imagerepos[image] = {}
+            for band in 'rgz':
+                PATTERN = images[image]
+                def getfilename(brick, PATTERN=PATTERN, band=band):
+                    return PATTERN % dict(brickid=brick.id, band=band)
+                imagerepos[image][band] = getfilename
+        return imagerepos
+
+    @staticmethod
+    def format_catalogue_filename(brick):
+        TRACTOR_FILENAME = 'tractor/tractor-%(brickid)d.fits'
+        return TRACTOR_FILENAME % dict(brickid=brick.id) 
+
+    @staticmethod
+    def parse_filename(filename, brickindex):
+        if not filename.endswith('.fits'): raise ValueError
+        return brickindex.search_by_id(
+                int(re.search('-([0123456789]+)\.', 
+                os.path.basename(filename)).group(1)))
+
+class EDR3(Schema):
+    """ Schema for EDR3.
+
+    """
+    BRICKS_FILENAME = 'decals-bricks.fits'
+    CATALOGUE_ALIASES = [('DECAM_EXTINCTION', 'DECAM_MW_TRANSMISSION', lambda x: 10**(x/-2.5))]
+    
+    @staticmethod
+    def format_image_filenames():
+        images = {
+        'depth': 'coadd/%(pre)s/%(brickname)s/decals-%(brickname)s-depth-%(band)s.fits',
+        'model': 'coadd/%(pre)s/%(brickname)s/decals-%(brickname)s-model-%(band)s.fits',
+        'image': 'coadd/%(pre)s/%(brickname)s/decals-%(brickname)s-image-%(band)s.fits',
+        }
+        imagerepos= {}
+        for image in images:
+            imagerepos[image] = {}
+            for band in 'rgz':
+                PATTERN = images[image]
+                def getfilename(brick, PATTERN=PATTERN, band=band):
+                    return PATTERN % dict(pre=brick.name[:3], brickname=brick.name, band=band)
+                imagerepos[image][band] = getfilename
+
+        return imagerepos
+
+    @staticmethod
+    def format_catalogue_filename(brick):
+        TRACTOR_FILENAME = 'tractor/%(pre)s/tractor-%(brickname)s.fits'
+        return TRACTOR_FILENAME % dict(pre=brick.name[:3], brickname=brick.name) 
+    @staticmethod
+    def parse_filename(filename, brickindex):
+        if not filename.endswith('.fits'): raise ValueError
+        brickname = re.search('-([p0123456789]+)\.', 
+                os.path.basename(filename)).group(1)
+        bid = brickindex.search_by_name(brickname)
+        return bid
+
+class EDR4(EDR3):
+    """ Schema for EDR4.
+        Changed to DECAM_MW_TRANSMISSION
+
+    """
+    CATALOGUE_ALIASES = []
+    pass
+
+class DR1(EDR4):
+    """ Schema for DR1. Same as EDR4.
+
+    """
+    pass
+
