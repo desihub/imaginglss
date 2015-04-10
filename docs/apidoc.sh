b94dfdf6ff039247bbdbb238cc192056700b5e45
Yu Feng <yfeng1@berkeley.edu>
2015-04-09 17:54:00 -0700

    per module

--- docs/apidoc.sh
+++ docs/apidoc.sh
@@ -3,4 +3,4 @@
 if ! python -c 'import numpydoc'; then easy_install --user numpydoc; fi
 if ! python -c 'import sphinx'; then easy_install --user sphinx; fi
 
-sphinx-apidoc -M -e -f -o . ../model
+sphinx-apidoc -M -e -f -o . ../model ../model/utils/sharedmem
