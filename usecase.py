437314772bd8100c2b737cf118f81817f4ac8b5b
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-05-03 14:49:10 -0700

    update the simple usecase example

--- usecase.py
+++ usecase.py
@@ -1,5 +1,5 @@
-from model.datarelease import DataRelease
-from model.imagerepo import ImageRepo
+from imaginglss import DataRelease
+from imaginglss.model.imagerepo import ImageRepo
 from pprint import pprint
 import numpy
 
@@ -7,40 +7,40 @@ dr = DataRelease()
 
 brick = dr.footprint.bricks[0]
 print dr.footprint
-print dr.images['image']['z'].metadata(brick)
+print dr.images['depth']['z'].metadata(brick)
 print '\n'.join(sorted([str(f) for f in dr.catalogue.dtype.fields]))
 
 def test398599():
     """ test image readout on brick-398599. 
     """
     dr = DataRelease()
-    brick = dr.footprint.bricks[0]
-    dr.images['image']['z'].preload([brick])
+    brick = dr.footprint.bricks[1]
+    dr.images['depth']['z'].preload([brick])
 
     print 'Testing on', brick
-    img2 = dr.images['image']['z'].open(brick)
+    img2 = dr.images['depth']['z'].open(brick)
 #    print img2
 #    assert (img2[300:-300, 300:-300] != 0).any()
 
-    print dr.images['image']['z'].get_filename(brick)
+    print dr.images['depth']['z'].get_filename(brick)
     y, x = numpy.indices(img2.shape)
     x = numpy.ravel(x) + 0.5
     y = numpy.ravel(y) + 0.5
-    coord = brick.revert(dr.images['image']['z'], (x, y))
+    coord = brick.revert(dr.images['depth']['z'], (x, y))
 
-    x2, y2 = brick.query(dr.images['image']['z'], coord)
+    x2, y2 = brick.query(dr.images['depth']['z'], coord)
     print x, x2
     print y, y2
     assert numpy.allclose(x, x2)
     assert numpy.allclose(y, y2)
 
 
-    img3 = brick.readout(coord, dr.images['image']['z'])
+    img3 = brick.readout(coord, dr.images['depth']['z'])
     diff = img3.reshape(img2.shape) - img2
     assert (diff[300:-300, 300:-300] == 0).all()
 
 
-    img = dr.readout(coord, dr.images['image']['z'])
+    img = dr.readout(coord, dr.images['depth']['z'])
     print 'brick readout passed'
     print 'found', (~numpy.isnan(img)).sum()
     diff = img.reshape(img2.shape) - img2
