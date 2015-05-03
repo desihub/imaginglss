eacc6298dc1d94f111ee7729386b45a986f7b522
Yu Feng <yfeng1@berkeley.edu>
2015-05-03 12:33:20 -0700

    split cut condition in cuts.

--- analysis/cuts.py
+++ analysis/cuts.py
@@ -57,7 +57,43 @@ def findlim(dr,sfd,coord,bands,sigma=5.0):
     return(ret)
     #
 
+class Cuts(object):
+    """ A convenient representation of a Cut 
+        
+        A :py:class:`Cut` object stores the cut expression
+        as a string, but can be called with the corresponding
+        replacements as keyward arguments. A boolean is returned.
 
+        Attributes
+        ----------
+        cuts : list
+            a list of python expressions as strings. 
+            For example,  ["rflux > 10**((22.5-23.00)/2.5)"]
+        
+        Parameters
+        ----------
+        *local : dict
+            the arguments to eval the cut expression. for example, rflux=[1.1, 2.3, ...]
+
+        Returns
+        -------
+        mask : array_like (Ncuts, Nitem)
+            True if item passed the cut, False if not. 
+
+    """
+    def __init__(self, cuts):
+        self.cuts = cuts
+
+    def __call__(self, **local):
+        return N.array([
+            eval(expr, local)
+            for expr in self.cuts])
+    def __iter__(self):
+        return iter(self.cuts)
+
+    def __repr__(self):
+        return "Cuts(%s)" % (self.expr)
+    
 class Fluxes:
     """
     A collection of helpful (static) methods to check whether an object's
@@ -68,44 +104,32 @@ class Fluxes:
     The requirement on BRICK_PRIMARY is handled elsewhere, these methods just
     look after the flux requirements.
     """
-    @staticmethod
-    def LRG(rflux,zflux,wflux):
-        mask = N.array([
-         rflux > 10**((22.5-23.00)/2.5),
-         zflux > 10**((22.5-20.56)/2.5),
-         wflux > 10**((22.5-19.50)/2.5),
-         zflux > 10**((1.6)       /2.5)*rflux,
-         wflux*rflux**(1.33-1)>zflux**1.33*10**(-0.33/2.5),
-        ]).T
-        return(mask)
-    #
-    @staticmethod
-    def ELG(gflux,rflux,zflux):
-        mask = N.array([
-         rflux    > 10**((22.5-23.4)/2.5),
-         zflux    > 10**((0.3)      /2.5)*rflux,
-         zflux    < 10**((1.5)      /2.5)*rflux,
-         rflux**2 < gflux*zflux*10**(-0.2/2.5),
-         zflux    > gflux*10**(1.2/2.5),
-        ]).T
-        return(mask)
-    #
-    @staticmethod
-    def QSO(gflux,rflux,w1flux,w2flux):
-        mask = N.array([
-         0.67*w1flux + 0.33*w2flux,
-         rflux > 10**((22.5-23.0)/2.5),
-         rflux < 10**((1.0)      /2.5)*gflux,
-         wflux*gflux**1.2 > 10**(2./2.5)*rflux**(1+1.2),
-        ]).T
-        return(mask)
-    @staticmethod
-    def BGS(rflux):
-        mask = N.array([
-         rflux > 10**((22.5-19.5)/2.5),
-        ]).T
-        return(mask)
-    #
+    LRG = Cuts([
+        "rflux > 10**((22.5-23.00)/2.5)",
+        "zflux > 10**((22.5-20.56)/2.5)",
+        "wflux > 10**((22.5-19.50)/2.5)",
+        "zflux > 10**((1.6)       /2.5)*rflux",
+        "wflux*rflux**(1.33-1)>zflux**1.33*10**(-0.33/2.5)",
+        ])
+
+    ELG = Cuts([
+        "rflux    > 10**((22.5-23.4)/2.5)",
+        "zflux    > 10**((0.3)      /2.5)*rflux",
+        "zflux    < 10**((1.5)      /2.5)*rflux",
+        "rflux**2 < gflux*zflux*10**(-0.2/2.5)",
+        "zflux    > gflux*10**(1.2/2.5)",
+        ])
+
+    QSO = Cuts([
+        "0.67*w1flux + 0.33*w2flux",
+        "rflux > 10**((22.5-23.0)/2.5)",
+        "rflux < 10**((1.0)      /2.5)*gflux",
+        "wflux*gflux**1.2 > 10**(2./2.5)*rflux**(1+1.2)",
+        ])
+
+    BGS = Cuts([
+        "rflux > 10**((22.5-19.5)/2.5)",
+        ])
  
 
 
@@ -120,43 +144,31 @@ class Completeness:
     MW transmission.
     Note: for a region to pass the N-sigma "noise" must be LESS THAN the
     required depth.
-    The requirements on colors are tricky to implement here.
+    The requirements on colors are tricky to implement here. We currently
+    require a 100% completeness.
+    
     """
-    @staticmethod
-    def LRG(rlim,zlim,wlim):
-        # This is not finished yet.
-        raise UnimplementedError
-        mask = N.array([
-         rlim<10**((22.5-23.00    )/2.5),
-         zlim<10**((22.5-20.56    )/2.5),
-         wlim<10**((22.5-19.50    )/2.5),
-         zlim<10**((22.5-23.00+1.6)/2.5),
-        ]).T
-        return(mask)
-    #
-    @staticmethod
-    def ELG(glim,rlim,zlim):
-        mask = N.array([
-         rlim < 10**((22.5-23.4        )/2.5),
-         zlim < 10**((22.5-23.4+0.3    )/2.5),
-         glim < 10**((22.5-23.4-1.5+0.2)/2.5),
-        ]).T
-        return(mask)
-    #
-    @staticmethod
-    def QSO(glim,rlim,w1lim,w2lim):
-        # This is not finished yet.
-        raise UnimplementedError
-        mask = N.array([
-         rlim<10**((22.5-23.00    )/2.5),
-         glim<10**((22.5-23.00-1.0)/2.5),
-        ]).T
-        return(mask)
-    #
-    @staticmethod
-    def BGS(rlim):
-        mask = N.array([
-         rlim<10**((22.5-19.5)/2.5),
-        ]).T
-        return(mask)
+
+    LRG = Cuts([
+        "rlim<10**((22.5-23.00    )/2.5)",
+        "zlim<10**((22.5-20.56    )/2.5)",
+        "zlim<10**((22.5-23.00+1.6)/2.5)",
+        "wlim<10**((22.5-19.50    )/2.5)",
+         "raise UnimplementedError",
+        ])
+
+    ELG = Cuts([
+        "glim < 10**((22.5-23.4-1.5+0.2)/2.5)",
+        "rlim < 10**((22.5-23.4        )/2.5)",
+        "zlim < 10**((22.5-23.4+0.3    )/2.5)",
+        ])
+
+    QSO = Cuts([
+        "raise UnimplementedError",
+        "rlim<10**((22.5-23.00    )/2.5)",
+        "glim<10**((22.5-23.00-1.0)/2.5)",
+        ])
+    BGS = Cuts([
+        "rlim<10**((22.5-19.5)/2.5)",
+        ])
     #
