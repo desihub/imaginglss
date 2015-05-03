6c40eaed45f0924ac16cc33f125249e75faf13e8
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-05-03 15:18:26 -0700

    add some toplevel description in API doc of model and utils.

---
+++ imaginglss/model/__init__.py
@@ -0,0 +1,7 @@
+"""
+
+:py:mod:`imaginglss.model` module models the DECALS imaging data releases.
+
+Please refer to :doc:`datamodel` for an overview.
+
+"""
---
+++ imaginglss/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+
+:py:mod:`imaginglss.utils` contains necessary code that supports
+the data model and scripts.
+
+Refer to :doc:`helpers`
+
+"""
