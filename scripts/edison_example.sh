8f54f002a82fe59ba333bff976f31f060c518c3f
Yu Feng <yfeng1@berkeley.edu>
2015-05-07 22:47:34 -0700

    update eidson example

--- scripts/edison_example.sh
+++ scripts/edison_example.sh
@@ -1,35 +1,48 @@
 #PBS -S /bin/bash
-#PBS -l mppwidth=24,walltime=00:05:00
+#PBS -l mppwidth=256,walltime=00:15:00
 #PBS -N Example
 #PBS -o Example.out
 #PBS -e Example.err
 #PBS -q debug
 #PBS -A m779
+
 #
 # An example "submit" script, to work on Edison at NERSC,
 # to read a catalog of Decals objects and print some
-# basic statics about them.
+# basic statics about them, with 256 mpi ranks.
 #
+# the script has been optimized for faster, consistent python startup
+#
+
+# Set the ImagingLSS configuration
+export DECALS_PY_CONFIG=/project/projectdirs/m779/imaginglss/dr1/dr1.conf.py
+
 cd $PBS_O_WORKDIR
 #
 # Load the modules we need.
-source /project/projectdirs/desi/software/modules/desi_environment.sh
-#
-# Set the path to the imaging data.
-export DECALS_IMAGING=/project/projectdirs/cosmo/work/decam/cats/edr3
-export DECALS_IMAGING=/project/projectdirs/cosmo/work/decam/release/edr/
-export DECALS_IMAGING=/project/projectdirs/cosmo/work/decam/release/dr1/
-export DECALS_CACHE=$GSCRATCH/desicache
-#
-# and set up the Python path...this is an example.
-export PYTHONPATH=${PYTHONPATH}:${PBS_O_WORKDIR}
-#
-export OMP_NUM_THREADS=24
-aprun -n 1 -d 24 python << EOF
-#import os.path; import sys; sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-from model.datarelease import DataRelease
-dr    = DataRelease()
-foot  = dr.footprint
-print foot
-EOF
+# use the faster $SCRATCH version of python
+module use /project/projectdirs/mpccc/usg/modulefiles/edison
+module load python mpi4py
+# but these packages have been pre-packaged to a payload to be
+# broadcast by python-mpi -bcast
+export PYTHONPAYLOAD=/project/projectdirs/m779/usg-python-2.7.9.tar.gz
+
+# loacation of the broadcast
+export PYTHON_MPI_HOME=/dev/shm
+
+# patch up python locations
+export PYTHONPATH=`echo $PYTHONPATH|sed -e "s;/scratch2/usg-python/;${PYTHON_MPI_HOME};g"`
+export PYTHONHOME=${PYTHON_MPI_HOME}/2.7.9
+
+
+# This is important to avoid python stating source files in $HOME
+# Also means all ~/.local packages are unavailable
+export PYTHONUSERBASE=$SCRATCH
+
+# No OMP threading
+export OMP_NUM_THREADS=1
+
+aprun -n 256 -d 1 /project/projectdirs/m779/python-mpi/python-mpi \
+    -bcast $PYTHONPAYLOAD \
+    ./select_elgs.py
 #
