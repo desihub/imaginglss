c71e980f95e3e1d47033026683a1facaa75baaa9
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-06-23 11:08:08 -0700

    flatten associative operators.

--- imaginglss/utils/npyquery.py
+++ imaginglss/utils/npyquery.py
@@ -18,70 +18,53 @@ Examples
 """
 import numpy
 
-opmap = {
-    '>=': numpy.greater_equal,
-    '<=': numpy.less_equal,
-    '>': numpy.greater,
-    '<': numpy.less,
-    '==': numpy.equal,
-    '!=': numpy.not_equal,
-    '*': numpy.multiply,
-    '/': numpy.divide,
-    '+': numpy.add,
-    '-': {
-        1: numpy.negative,
-        2: numpy.subtract,
-        },
-    '~': numpy.bitwise_not,
-    '&': numpy.bitwise_and,
-    '|': numpy.bitwise_or,
-    '^': numpy.bitwise_xor,
-    '**' : numpy.power,
-    'sin': numpy.sin,
-    'cos': numpy.cos,
-    'exp': numpy.exp,
-    'log10': numpy.log10,
-    'log': numpy.log,
-}
 class Node(object):
     def __le__(self, other):
-        return Expr("<=", [self, other])
+        return Expr("<=", numpy.less_equal, [self, other])
     def __lt__(self, other):
-        return Expr("<", [self, other])
+        return Expr("<", numpy.less, [self, other])
     def __eq__(self, other):
-        return Expr("==", [self, other])
+        return Expr("==", numpy.equal, [self, other])
     def __gt__(self, other):
-        return Expr(">", [self, other])
+        return Expr(">", numpy.greater, [self, other])
     def __ge__(self, other):
-        return Expr(">=", [self, other])
+        return Expr(">=", numpy.greater_equal, [self, other])
     def __and__(self, other):
-        return Expr("&", [self, other])
+        return Expr("&", numpy.bitwise_and, [self, other])
     def __or__(self, other):
-        return Expr("|", [self, other])
+        return Expr("|", numpy.bitwise_or, [self, other])
     def __xor__(self, other):
-        return Expr("^", [self, other])
+        return Expr("^", numpy.bitwise_xor, [self, other])
     def __invert__(self):
-        return Expr("!", [self])
+        return Expr("~", numpy.bitwise_not, [self])
     def __pow__(self, other):
-        return Expr("**", [self, other])
+        return Expr("**", numpy.power, [self, other])
     def __rpow__(self, other):
-        return Expr("**", [other, self])
+        return Expr("**", numpy.power, [other, self])
     def __mul__(self, other):
-        return Expr("*", [self, other])
+        return Expr("*", numpy.multiply, [self, other])
     def __div__(self, other):
-        return Expr("/", [self, other])
+        return Expr("/", numpy.divide, [self, other])
     def __add__(self, other):
-        return Expr("+", [self, other])
+        return Expr("+", numpy.add, [self, other])
     def __neg__(self):
-        return Expr("-", [self])
+        return Expr("-", numpy.negative, [self])
     def __sub__(self, other):
-        return Expr("-", [self, other])
+        return Expr("-", numpy.subtract, [self, other])
     def __mod__(self, other):
-        return Expr("%", [self, other])
+        return Expr("%", numpy.remainder, [self, other])
     def __getitem__(self, index):
         return GetItem(self, index)
     def sin(self):
-        return Expr("sin", [self])
+        return Expr("sin", numpy.sin, [self])
+    def cos(self):
+        return Expr("cos", numpy.cos, [self])
+    def tan(self):
+        return Expr("tan", numpy.tan, [self])
+    def log(self):
+        return Expr("log", numpy.log, [self])
+    def log10(self):
+        return Expr("log10", numpy.log10, [self])
     def __call__(self, array):
         return array[self.visit(array)]
     def visit(self, array):
@@ -132,44 +115,76 @@ class Column(Node):
         return array[self.column]
 
 class Expr(Node):
-    def __init__(self, operator, operands):
+    def __init__(self, operator, function, operands):
         self.operator = operator
+        self.function = function
         operands = [
             a if isinstance(a, Node)
             else Literal(a)
             for a in operands
         ]
+
         self.operands = operands
+        self.flatten()
+        
+    def is_associative(self):
+        if not isinstance(self.function, numpy.ufunc):
+            return False
+        if self.function.identity is not None:
+            return True
+        return False
+
+    def __iter__(self):
+        return iter(self.operands)
+
+    def flatten(self):
+        if not self.is_associative(): return
+        o = []
+        for a in self.operands:
+            if not isinstance(a, Expr):
+                o.append(a)
+                continue
+            if a.function != self.function:
+                o.append(a)
+                continue
+            else:
+                o.extend(a.operands)
+        self.operands = o
 
     def __repr__(self):
-        if len(self.operands) == 2:
-            a, b = self.operands
-            return "(%s %s %s)" % (repr(a), self.operator, repr(b))
+        if len(self.operands) >= 2:
+            return "(%s)" % (' ' + self.operator + ' ').join([repr(a) for a in self.operands])
         elif len(self.operands) == 1:
             [a] = self.operands
             return "(%s %s)" % (self.operator, repr(a))
         else:
             raise ValueError
+
     def visit(self, array):
         ops = [a.visit(array) for a in self.operands]
-        function = opmap[self.operator]
-        if isinstance(function, dict):
-            function = function[len(ops)]
-        return function(* ops)
+        if self.is_associative():
+            return self.function.reduce(ops)
+        else:
+            return self.function(*ops)
 
 def test():    
     d = numpy.dtype([
         ('BlackholeMass', 'f4'), 
-        ('Position', ('f4', 3))])
+        ('PhaseOfMoon', 'f4'), 
+        ('Position', ('f4', 3)),
+])
 
-    data = numpy.zeros(4, dtype=d)
-    data['BlackholeMass'][:] = numpy.arange(4)
-    data['Position'][:] = numpy.arange(12).reshape(4, -1)
-    query  = (Column('BlackholeMass') > 2.0)
-    query &= (Column('BlackholeMass') < 4.0)
-    query &= (Column('Position')[:, 2] > 1.0)
+    data = numpy.zeros(5, dtype=d)
+    data['BlackholeMass'][:] = numpy.arange(len(data))
+    data['PhaseOfMoon'][:] = numpy.linspace(0, 1, len(data), endpoint=True)
+    data['Position'][:] = numpy.arange(data['Position'].size).reshape(len(data), -1)
+    query  = (Column('BlackholeMass') > 0.0)
+    query &= (Column('BlackholeMass') < 5.0)
+    query &= (Column('Position')[:, 2] > 0.0) | (Column('Position')[:, 1] < 0.0)
+    query &= (numpy.sin(Column('PhaseOfMoon') * (2 * numpy.pi)) < 0.1)
     print query
     print query(data)
-
+    for sub in query:
+        print sub, sub.visit(data)
 if __name__ == '__main__':
     test()
