04c1223fda2e31952bce5fb14261763eb166f878
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-02-10 13:16:11 -0800

    Update tests.

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
 
-python scripts/build_cache.py --conf=testdata/dr2-mini/dr2.conf.py
-python scripts/select_objs.py --conf=testdata/dr2-mini/dr2.conf.py QSO QSO.txt
-python scripts/make_random.py --conf=testdata/dr2-mini/dr2.conf.py 1000 QSO QSO.txt
+python scripts/build_cache.py --conf=testdata/dr2-mini/dr2.conf.py || exit 1
+python scripts/select_objs.py --conf=testdata/dr2-mini/dr2.conf.py QSO QSO.txt || exit 1
+python scripts/make_random.py --conf=testdata/dr2-mini/dr2.conf.py 1000 QSO QSO.txt || exit 1
