2c8fd7c0664645c16838ae95773383980cd85828
Ellie Kitanidis <elliek@berkeley.edu>
2016-03-09 14:01:21 -0800

    Added tighter constraint on ELG z-band.

--- imaginglss/analysis/completeness.py
+++ imaginglss/analysis/completeness.py
@@ -61,9 +61,10 @@ def ELG(sigma):
             band. ( :math:`n \\sigma`)
 
     """
-    ELG =  sigma['g'] * G_LIMIT < 10**((22.5-23.4-1.5+0.2)/2.5)
-    ELG &= sigma['r'] * R_LIMIT < 10**((22.5-23.4        )/2.5)
-    ELG &= sigma['z'] * Z_LIMIT < 10**((22.5-23.4+0.3    )/2.5)
+    ELG = sigma['r'] * R_LIMIT < 10**((22.5-23.4        )/2.5)
+    ELG &=  sigma['z'] * Z_LIMIT < 10**((22.5-23.4+0.3    )/2.5)
+    ELG &=  sigma['g'] * G_LIMIT < 10**((22.5-23.4-1.5+0.2)/2.5)
+    ELG &=  sigma['g'] * G_LIMIT < 10**((22.5-23.4-1.2+0.3)/2.5
     return ELG
 ELG.bands = 'grz'
 __all__.append("ELG")
