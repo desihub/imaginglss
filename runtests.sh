c38d8613678ea08313db7bb8284b420e4c17d45e
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-03-23 16:50:13 -0700

    update test cases to new file names.

--- runtests.sh
+++ runtests.sh
@@ -1,14 +1,14 @@
 
-python scripts/build_cache.py --conf=testdata/dr2-mini/dr2.conf.py || exit 1
-python scripts/select_objs.py --conf=testdata/dr2-mini/dr2.conf.py QSO QSO.txt || exit 1
+python scripts/imglss-mpi-build-cache.py --conf=testdata/dr2-mini/dr2.conf.py || exit 1
+python scripts/imglss-mpi-select-objects.py --conf=testdata/dr2-mini/dr2.conf.py QSO QSO.txt || exit 1
 head QSO.txt.NOISES
 head QSO.txt.FLUXES
-python scripts/select_objs.py --conf=testdata/dr2-mini/dr2.conf.py QSO QSO.hdf5 || exit 1
-python scripts/make_random.py --conf=testdata/dr2-mini/dr2.conf.py 1000 QSO-random.txt || exit 1
+python scripts/imglss-mpi-select-objects.py --conf=testdata/dr2-mini/dr2.conf.py QSO QSO.hdf5 || exit 1
+python scripts/imglss-mpi-make-random.py --conf=testdata/dr2-mini/dr2.conf.py 1000 QSO-random.txt || exit 1
 head QSO-random.txt.NOISES
-python scripts/make_random.py --conf=testdata/dr2-mini/dr2.conf.py 1000 QSO-random.hdf5 || exit 1
+python scripts/imglss-mpi-make-random.py --conf=testdata/dr2-mini/dr2.conf.py 1000 QSO-random.hdf5 || exit 1
 
-python scripts/query_completeness.py --conf=testdata/dr2-mini/dr2.conf.py QSO QSO-random.txt QSO.txt QSO-random.txt || exit 1
+python scripts/imglss-query-completeness.py --conf=testdata/dr2-mini/dr2.conf.py QSO QSO-random.txt QSO.txt QSO-random.txt || exit 1
 head QSO-random.txt.FC
-python scripts/query_tycho_veto.py --conf=testdata/dr2-mini/dr2.conf.py QSO-random.txt QSO-random.txt|| exit 1
+python scripts/imglss-query-tycho-veto.py --conf=testdata/dr2-mini/dr2.conf.py QSO-random.txt QSO-random.txt|| exit 1
 head QSO-random.txt.TYCHOVETO
