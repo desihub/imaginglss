f3e842e0d39c73ef32a1cbb70a86bc342fbcdc6b
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-04-19 12:06:18 -0700

    Add QSOd for david's size based QSO cut.
    This is more inclusive and supposingly, more uniform.

--- imaginglss/analysis/targetselection.py
+++ imaginglss/analysis/targetselection.py
@@ -28,16 +28,12 @@ __email__  = "yfeng1@berkeley.edu or mjwhite@lbl.gov"
 from imaginglss.model.columnnames import *
 from imaginglss.utils.npyquery import Max, Min
 
-__all__ = []
-
 LRG =  BRICK_PRIMARY != 0
 LRG &= ZFLUX > 10**((22.5-20.46)/2.5)
 LRG &= ZFLUX > RFLUX * 10**(1.5/2.5)
 LRG &= W1FLUX > 10**((22.5-19.35)/2.5)
 LRG &= W1FLUX * RFLUX ** (1.8-1) > ZFLUX**1.8 * 10**(-1.0/2.5)
 LRG.bands = 'rz'
-
-__all__.append("LRG")
 
 ELG =  BRICK_PRIMARY != 0
 ELG &= RFLUX > 10**((22.5-23.4)/2.5)
@@ -48,8 +44,7 @@ ELG &= ZFLUX**1.2 < GFLUX * RFLUX**0.2 * 10**(1.6/2.5)
 #ELG &= Max(SHAPEDEV_R, SHAPEEXP_R) < 1.5
 ELG.bands = 'rgz'
 
-__all__.append("ELG")
-
+# QSO by colors only
 QSOC  = RFLUX > 10**((22.5-23.0)/2.5)
 QSOC &= GRZFLUX < 10**((22.5-17.0)/2.5)
 QSOC &= RFLUX < 10**(1.3/2.5) * GFLUX
@@ -63,28 +58,45 @@ QSOC &= SNRW2 > 2
 QSO = BRICK_PRIMARY != 0
 QSO &= QSOC
 QSO &= TYPE == 'PSF '
-
 QSO.bands = 'rgz'
 
-__all__.append("QSO")
+# David's variant of QSO
+QSOd = BRICK_PRIMARY != 0
+QSOd &= QSOC
+QSOd &= Max(SHAPEDEV_R, SHAPEEXP_R) < 0.5
+QSOd.bands = 'rgz'
 
 BGS =  BRICK_PRIMARY != 0
-""" BGS Cut """
-
 BGS &= TYPE != 'PSF'
-BGS &=  RFLUX > 10**((22.5-19.5)/2.5)
+BGS &= RFLUX > 10**((22.5-19.5)/2.5)
 BGS.bands = 'r'
-__all__.append("BGS")
 
+__all__ = []
 # now we try to import a local version the file
 def _local():
+    global __all__
+
     import os
+
     local = os.path.join(os.path.dirname(__file__), 'local-%s' % os.path.basename(__file__))
     if local.endswith('.pyc'): local = local[:-1]
     if os.path.exists(local):
         script = open(local, 'r').read()
         exec(compile(script, local, 'exec'), globals())
-    del globals()['_local']
+    g = globals()
+
+    # This will filter out names that does not appear to be a target type.
+    import imaginglss.model.columnnames as columnnames
+    blacklist = dir(columnnames)
+    for k in g.keys():
+        if k in blacklist:
+            continue
+        if k in ['Min', 'Max']:
+            continue
+        if k.startswith('_'):
+            continue
+        __all__.append(k)
+
 _local()
 
 
