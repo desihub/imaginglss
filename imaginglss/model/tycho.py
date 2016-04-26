384ac47cc42366efb59ca474f516164d365eb5cc
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-04-25 17:21:48 -0700

    Update to tycho2 catalogue for veto.
    Tycho2 has significantly more stars and seems to be more useful than
    tycho1 in rejecting stars from the QSO TS type.
    
    The code will stop working and complain the tycho catalogue was wrong.
    it will give a path for downloading the correct file (which were
    found from legacysurvey DR2 directory on NERSC)
    
    dr2-mini.tar.gz (used in tests) has been updated to use Tycho2.
    
    According to private communication with @djschlegel bstar-merged that
    he uses for stripe-82 TS is roughly the same thing as Tycho2.

--- imaginglss/model/tycho.py
+++ imaginglss/model/tycho.py
@@ -6,12 +6,14 @@ __version__ = "1.0"
 __email__  = "yfeng1@berkeley.edu or mjwhite@lbl.gov"
 
 
-class Tycho(numpy.ndarray):
+class Tycho1(numpy.ndarray):
     """ Representing a Tycho catalogue that
         is used to veto objects near stars 
     """
     def __new__(kls, path):
         data = fits.read_table(path).view(type=kls)
+        if 'VMAG' not in data.dtype.names:
+            raise ValueError("The file at `%s` is not a legacysurvey Tycho catalogue. Retrive from https://s3-us-west-1.amazonaws.com/imaginglss/tycho.tar.gz. (And unzip)" % path)
         self = numpy.empty(len(data),
             dtype=[
                 ('RA', 'f8'),
@@ -36,3 +38,36 @@ class Tycho(numpy.ndarray):
 
         return self.view(type=kls)
 
+class Tycho2(numpy.ndarray):
+    """ Representing a Tycho catalogue that
+        is used to veto objects near stars 
+    """
+    def __new__(kls, path):
+        data = fits.read_table(path).view(type=kls)
+        if 'MAG_VT' not in data.dtype.names:
+            raise ValueError("The file at `%s` is not a legacysurvey Tycho2 catalogue. Retrive from https://s3-us-west-1.amazonaws.com/imaginglss/tycho2.tar.gz. (And unzip)" % path)
+        self = numpy.empty(len(data),
+            dtype=[
+                ('RA', 'f8'),
+                ('DEC', 'f8'),
+                ('VTMAG', 'f8'),
+                ('VMAG', 'f8'),
+                ('BMAG', 'f8'),
+                ('BTMAG', 'f8'),
+                ('VARFLAG', 'i8'),
+                ])
+        self['RA'] = data['RA']
+        self['DEC'] = data['DEC']
+        self['VTMAG'] = data['MAG_VT']
+        self['BTMAG'] = data['MAG_BT']
+        vt = self['VTMAG']
+        bt = self['BTMAG']
+        b = vt - 0.09 * (bt - vt)
+        v = b - 0.85 * (bt - vt)
+        self['VMAG'] = v
+        self['BMAG'] = b
+
+        return self.view(type=kls)
+
+# Use tycho2 catalogue.
+Tycho = Tycho2
