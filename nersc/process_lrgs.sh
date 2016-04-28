6bcee6b93d99a02393a18a9a51a12c1d1fe546ab
Yu Feng <yfeng1@berkeley.edu>
2016-04-27 17:20:33 -0700

    Add sample batch and non-batch scripts

---
+++ nersc/process_lrgs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CONF=/project/projectdirs/m779/yfeng1/imaginglss/dr2.conf.py
+
+python ../scripts/imglss-query-tycho-veto.py LRG-RANDOM.hdf5 --conf $CONF
+python ../scripts/imglss-query-tycho-veto.py LRG.hdf5 --conf $CONF
+python ../scripts/imglss-query-completeness.py --conf $CONF \
+        --use-tycho-veto=BOSS_DR9 LRG LRG.hdf5 LRG.hdf5
+python ../scripts/imglss-query-completeness.py --conf $CONF \
+        --use-tycho-veto=BOSS_DR9 LRG LRG.hdf5 LRG-RANDOM.hdf5
+python '../scripts/imglss-export-text.py' --conf $CONF \
+        --use-tycho-veto=BOSS_DR9 --bands r g z W1 W2 -- LRG.hdf5 LRG.txt
+python '../scripts/imglss-export-text.py' --conf $CONF \
+        --use-tycho-veto=BOSS_DR9 --bands r g z W1 W2 -- LRG-RANDOM.hdf5 LRG-RANDOM.txt
---
+++ nersc/process_qsos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CONF=/project/projectdirs/m779/yfeng1/imaginglss/dr2.conf.py
+
+python ../scripts/imglss-query-tycho-veto.py QSO-RANDOM.hdf5 --conf $CONF
+python ../scripts/imglss-query-tycho-veto.py QSO.hdf5 --conf $CONF
+python ../scripts/imglss-query-completeness.py --conf $CONF \
+        --use-tycho-veto=BOSS_DR9 QSO QSO.hdf5 QSO.hdf5
+python ../scripts/imglss-query-completeness.py --conf $CONF \
+        --use-tycho-veto=BOSS_DR9 QSO QSO.hdf5 QSO-RANDOM.hdf5
+python '../scripts/imglss-export-text.py' --conf $CONF \
+        --use-tycho-veto=BOSS_DR9 --bands r g z W1 W2 -- QSO.hdf5 QSO.txt
+python '../scripts/imglss-export-text.py' --conf $CONF \
+        --use-tycho-veto=BOSS_DR9 --bands r g z W1 W2 -- QSO-RANDOM.hdf5 QSO-RANDOM.txt
