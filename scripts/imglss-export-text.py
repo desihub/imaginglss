46a4d679443572015c973c4de1cfee172c6cb641
Yu Feng <yfeng1@berkeley.edu>
2016-05-03 10:46:54 -0700

    Add reminders in imglss-export-text.

--- scripts/imglss-export-text.py
+++ scripts/imglss-export-text.py
@@ -14,11 +14,11 @@ ap.add_argument("--conf", default=None,
         help="Path to the imaginglss config file, default is from DECALS_PY_CONFIG")
 
 allvetos = [i for i in dir(tycho_veto) if not str(i).startswith( '_' )]
-ap.add_argument("--use-tycho-veto", type=str, choices=allvetos, default=None)
-ap.add_argument("--bands", nargs='+', type=str, choices=product.bands.keys(), default=['r', 'g', 'z'])
-ap.add_argument("--sigma-z", type=float, default=3.0)
-ap.add_argument("--sigma-g", type=float, default=5.0)
-ap.add_argument("--sigma-r", type=float, default=5.0)
+ap.add_argument("--use-tycho-veto", type=str, choices=allvetos, default=None, help="Apply tycho veto, must run imglss-query-tycho-veto first!")
+ap.add_argument("--bands", nargs='+', type=str, choices=product.bands.keys(), default=['r', 'g', 'z'], help="Bands to export in the text file")
+ap.add_argument("--sigma-z", type=float, default=3.0, help="apply a confidence cut in z")
+ap.add_argument("--sigma-g", type=float, default=5.0, help="apply a confidence cut in g")
+ap.add_argument("--sigma-r", type=float, default=5.0, help="apply a confidence cut in r")
 
 ns = ap.parse_args()
 decals = DECALS(ns.conf)
@@ -41,15 +41,23 @@ with h5py.File(ns.catalogue, 'r') as ff:
     mask = np.ones(len(ff['RA'][:]), dtype='?')
 
     if 'CONFIDENCE' in ff:
+        # this is a catalogue, apply confidence cut.
         confmask = apply_confcut(ff['CONFIDENCE'][:], confcut)
         mask &= confmask
 
     if ns.use_tycho_veto is not None:
+        if not 'TYCHO_VETO' in ff:
+            raise KeyError("TYCHO_VETO dataset is not found. Run imglss-query-tycho-veto on `%s` first." % ns.catalogue)
         vetomask = ~ff['TYCHO_VETO'][ns.use_tycho_veto]
+            
         mask &= vetomask
 
     ra = ff['RA'][:][mask]
     dec = ff['DEC'][:][mask]
+
+    if not 'COMPLETENESS' in ff:
+        raise KeyError("COMPLETENESS dataset is not found. Run imglss-query-completeness on `%s` first." % ns.catalogue)
+        
     fc = ff['COMPLETENESS'][:][mask]
     h = ['RA', 'DEC', 'COMPETENESS']
     l = [ra, dec, fc]
@@ -57,6 +65,7 @@ with h5py.File(ns.catalogue, 'r') as ff:
     for band in ns.bands:
         iband = product.bands[band]
         if 'INTRINSIC_FLUX' in ff:
+            # this is a catalogue, dump flux as well as noise
             h.append(band + 'flux')
             l.append(ff['INTRINSIC_FLUX'][:, iband][mask])
         h.append(band + 'noise')
