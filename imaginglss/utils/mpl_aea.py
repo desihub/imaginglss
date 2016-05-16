b375d0a4af6abf30d3781d4f6796a1d993afd10c
Yu Feng <yfeng1@berkeley.edu>
2016-05-16 12:13:55 -0700

    Improvements on painting with the TriMesh.

--- imaginglss/utils/mpl_aea.py
+++ imaginglss/utils/mpl_aea.py
@@ -37,7 +37,9 @@ import matplotlib
 from matplotlib.axes import Axes
 from matplotlib.patches import Rectangle, Polygon
 from matplotlib.path import Path
-from matplotlib.collections import PolyCollection, Collection, allow_rasterization
+from matplotlib.collections import PolyCollection, TriMesh
+from matplotlib.tri.triangulation import Triangulation
+
 from matplotlib.ticker import NullLocator, Formatter, FixedLocator, MaxNLocator
 from matplotlib.transforms import Affine2D, BboxTransformTo, Transform, blended_transform_factory, Bbox
 from matplotlib.projections import register_projection
@@ -470,12 +472,13 @@ class SkymapperAxes(Axes):
             coll = HealpixCollection(map, mask, 
                     transform=self.transData, **defaults)
         else:
-            raise TypeError('gouraud does not work. We do not know colors on the vertices')
-
+            coll = HealpixTriCollection(map, mask, transform=self.transData, **defaults)
+        
         coll.set_clim(vmin=vmin, vmax=vmax)
         self.add_collection(coll)
         self._sci(coll)
         self.autoscale_view(tight=True)
+
         return coll
 
     def mapcontour(self, map, mask=None, nest=False, **kwargs):
@@ -787,6 +790,123 @@ class HealpixCollection(PolyCollection):
         vmax = (360, 90)
         return Bbox((vmin, vmax))
 
+import matplotlib.transforms as mtransforms
+from matplotlib.externals import six
+from matplotlib.externals.six.moves import zip
+import warnings
+import numpy as np
+import numpy.ma as ma
+import matplotlib as mpl
+import matplotlib.cbook as cbook
+import matplotlib.colors as mcolors
+import matplotlib.cm as cm
+from matplotlib import docstring
+import matplotlib.transforms as transforms
+import matplotlib.artist as artist
+from matplotlib.artist import allow_rasterization
+import matplotlib.backend_bases as backend_bases
+import matplotlib.path as mpath
+from matplotlib import _path
+import matplotlib.mlab as mlab
+import matplotlib.lines as mlines
+from matplotlib.collections import Collection
+
+class HealpixTriCollection(Collection):
+    """
+    Class for the efficient drawing of a triangular mesh using
+    Gouraud shading.
+
+    A triangular mesh is a :class:`~matplotlib.tri.Triangulation`
+    object.
+    """
+    def __init__(self, map, mask, nest=False, **kwargs):
+        Collection.__init__(self, **kwargs)
+        nside = healpix.npix2nside(len(map))
+        # remove the first axes
+        verts = pix2tri(nside, mask.nonzero()[0]).reshape(-1, 3, 2)
+        c = np.ones((verts.shape[0], verts.shape[1])) * np.repeat(map[mask][:, None], 2, axis=0)
+
+        self._verts = verts
+        self._shading = 'gouraud'
+        self._is_filled = True
+        self.set_array(c.reshape(-1))
+        
+    def get_paths(self):
+        if self._paths is None:
+            self.set_paths()
+        return self._paths
+
+    def set_paths(self):
+        self._paths = self.convert_mesh_to_paths(self._verts)
+
+    @staticmethod
+    def convert_mesh_to_paths(verts):
+        """
+        Converts a given mesh into a sequence of
+        :class:`matplotlib.path.Path` objects for easier rendering by
+        backends that do not directly support meshes.
+
+        This function is primarily of use to backend implementers.
+        """
+        Path = mpath.Path
+        return [Path(x) for x in verts]
+
+    @allow_rasterization
+    def draw(self, renderer):
+        if not self.get_visible():
+            return
+        renderer.open_group(self.__class__.__name__)
+        transform = self.get_transform()
+
+        # Get a list of triangles and the color at each vertex.
+        
+        verts = self._verts
+        
+        self.update_scalarmappable()
+        colors = self._facecolors.reshape(-1, 3, 4)
+        
+        oldshape = list(verts.shape)
+        
+        verts = transform.transform(verts.reshape(-1, 2)).reshape(oldshape)
+
+        gc = renderer.new_gc()
+        self._set_gc_clip(gc)
+        gc.set_linewidth(self.get_linewidth()[0])
+        renderer.draw_gouraud_triangles(gc, verts, colors, mtransforms.IdentityTransform())
+        gc.restore()
+        renderer.close_group(self.__class__.__name__)
+
+    def get_datalim(self, transData):
+        """ The data lim of a healpix collection.
+        """ 
+        # FIXME: it is currently set to the full sky.
+        #    This could have been trimmed down. 
+        #    We want to set xlim smartly such that the largest
+        #    empty region is chopped off. I think it is possible, by
+        #    doing a histogram in ra, for example. 
+        vmin = (0, -90)
+        vmax = (360, 90)
+        return Bbox((vmin, vmax))
+
+
+def _wrap360(phi, dir='left'):
+    phi[np.abs(phi) < 1e-9] = 0
+    if dir == 'left':
+        ref = phi.min(axis=-1)
+    else:
+        ref = phi.max(axis=-1)
+#    print('ref', ref, phi, ref % 360 - ref)
+    diff = (ref % 360) - ref 
+    phi = phi + diff[:, None]
+    
+    #diff = phi - ref[:, None] 
+    #print('great', (diff > 180).sum())
+    #diff[diff > 180] -= 360 
+    #print('less', (diff < -180).sum())
+    #diff[diff < -180] += 360
+    #phi = ref[:, None] + diff
+    return phi 
+
 # a few helper functions talking to healpy/healpix.
 def pix2quad(nside, pix, nest=False):
     """Generate healpix quad vertices for pixels where mask is True
@@ -807,12 +927,12 @@ def pix2quad(nside, pix, nest=False):
     theta, phi = healpix.vertices(nside, pix)
     theta = np.degrees(theta)
     phi = np.degrees(phi)
-    diff = phi - phi[:, 0][:, None] 
-    diff[diff > 180] -= 360 
-    diff[diff < -180] += 360
-    phi = phi[:, 0][:, None] + diff
-    vertices[:,:,0] = phi
-    vertices[:,:,1] = 90.0 - theta
+
+    vertices[:, :, 0] = phi
+    vertices[:, :, 1] = 90.0 - theta
+
+    # ensure objects are in the same image plane.
+    vertices[:, :, 0] = _wrap360(phi, 'right')
 
     return vertices
 
@@ -836,15 +956,11 @@ def pix2tri(nside, pix, nest=False):
     theta, phi = healpix.vertices(nside, pix)
     theta = np.degrees(theta)
     phi = np.degrees(phi)
-    diff = phi - phi[:, 0][:, None]
-    diff[diff > 180] -= 360
-    diff[diff < -180] += 360
-    phi = phi[:, 0][:, None] + diff
 
-    vertices[:, 0, :, 0] = phi[:, [2, 1, 0]]
-    vertices[:, 0, :, 1] = 90.0 - theta[:, [2, 1, 0]]
-    vertices[:, 1, :, 0] = phi[:, [0, 3, 2]]
-    vertices[:, 1, :, 1] = 90.0 - theta[:, [0, 3, 2]]
+    vertices[:, 0, :, 0] = _wrap360(phi[:, [0, 1, 3]], 'left')
+    vertices[:, 0, :, 1] = 90.0 - theta[:, [0, 1, 3]]
+    vertices[:, 1, :, 0] = _wrap360(phi[:, [1, 2, 3]], 'right')
+    vertices[:, 1, :, 1] = 90.0 - theta[:, [1, 2, 3]]
 
     return vertices
 
@@ -1076,6 +1192,7 @@ def create_healpix():
                 for each (nside, pix) pair, a four-vector of theta, and
                 a four-vector of phi is returned, corresponding to
                 the theta, phi of each vertex of the pixel boundary.
+                (left, bottom, right, top)
         """
         nside, pix = numpy.lib.stride_tricks.broadcast_arrays(nside, pix)
         x = numpy.zeros(nside.shape, dtype=('f8', 4))
@@ -1140,6 +1257,9 @@ if __name__ == '__main__':
 
     ax.tripcolor(ra, dec, ra*dec)
     fig.colorbar(ax._gci())
+
+    ra = np.random.uniform(size=1000, low=180, high=200)
+    dec = np.random.uniform(size=1000, low=-50, high=50)
 
     ax.set_meridian_grid(30)
     ax.set_parallel_grid(30)
