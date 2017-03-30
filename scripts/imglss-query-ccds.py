ff65ab4fe1918e6a16a15cc8581f3849789a625d
Yu Feng <rainwoodman@gmail.com>
2017-03-29 22:52:21 -0700

    support multiple ccdattrs.

--- scripts/imglss-query-ccds.py
+++ scripts/imglss-query-ccds.py
@@ -23,7 +23,7 @@ cli.add_argument("query",
 cli.add_argument("ccdfile",
         help="survey ccd fits file; this is the path to the -decals file. we also need the -nondecals and -extras file.")
 
-cli.add_argument("ccdattr", type=lambda x: x.upper(),
+cli.add_argument("ccdattrs", type=lambda x: x.upper(), nargs="+",
         help="column name to query ")
 
 ns = cli.parse_args()
@@ -41,14 +41,19 @@ def main():
 
     print("size of query is %d" % len(RA))
     print("number of CCDS is %d" % len(ccdtable))
-
-    if ns.ccdattr not in ccdtable.data.dtype.names and ns.ccdattr != 'NEXP':
+    attrnames = list(ccdtable.data.dtype.names) + ['NEXP']
+    if len(ns.ccdattrs) == 0 or \
+       any(attr not in attrnames for attr in ns.ccdattrs):
         raise RuntimeError("ccdattr not found, available ones are %s"
-                    % str(list(ccdtable.data.dtype.names) + ['NEXP']))
-        
-    r1 = np.zeros_like(RA)
-    r2 = np.zeros_like(RA)
-    N = np.zeros_like(RA)
+                    % str(attrnames))
+
+    attrdtype = np.dtype([
+        (attr, 'f8')
+            for attr in ns.ccdattrs])
+
+    r1 = np.zeros_like(RA, dtype=attrdtype)
+    r2 = np.zeros_like(RA, dtype=attrdtype)
+    N = np.zeros_like(RA, dtype='f8')
 
     querytree = points(RA, DEC).tree
     
@@ -57,30 +62,32 @@ def main():
         i = i[mask]
         j = j[mask]
 
-        if ns.ccdattr != 'NEXP':
-            v = ccdtable.data[ns.ccdattr][i]
-            np.add.at(r1, j, v)
-            np.add.at(r2, j, v ** 2)
+        v = ccdtable.data[i]
+        for attr in ns.ccdattrs:
+            if attr != 'NEXP':
+                np.add.at(r1[attr], j, v[attr])
+                np.add.at(r2[attr], j, v[attr] ** 2)
         np.add.at(N, j, 1)
 
     ccdtree.root.enum(querytree.root, np.radians(0.2), process)
 
-    COLUMNNAME = 'CCD-%s' % ns.ccdattr
+    for attr in ns.ccdattrs:
+        if attr != 'NEXP':
+            r1[attr] /= N
+        else:
+            r1[attr] = N
 
-    if ns.ccdattr != 'NEXP':
-        r1 /= N
-    else:
-        r1 = N
-
-    print('mean and std of %s from the query is %g %g' % (ns.ccdattr, r1.mean(), r1.std()))
+        print('mean and std of %s from the query is %g %g' % (attr, r1[attr].mean(), r1[attr].std()))
 
     with h5py.File(ns.query, 'r+') as ff:
-        if COLUMNNAME in ff:
-            del ff[COLUMNNAME]
-        ds = ff.create_dataset(COLUMNNAME, data=r1)
-        ds.attrs.update(cli.prune_namespace(ns))
+        for attrname in ns.ccdattrs:
+            COLUMNNAME = 'CCD-%s' % attrname
+            if COLUMNNAME in ff:
+                del ff[COLUMNNAME]
+            ds = ff.create_dataset(COLUMNNAME, data=r1[attrname])
+            ds.attrs.update(cli.prune_namespace(ns))
 
-    print('written as %s in %s' % ( COLUMNNAME, ns.query))
+            print('written %s as %s in %s' % (attrname, COLUMNNAME, ns.query))
 
 if __name__ == "__main__":
     main()
