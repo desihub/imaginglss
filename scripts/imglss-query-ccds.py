f7640f9619449d2a9787755a076dbd30f5ddb989
Ellie Kitanidis <elliek@berkeley.edu>
2017-12-03 13:16:26 -0800

    count nexp in each band

--- scripts/imglss-query-ccds.py
+++ scripts/imglss-query-ccds.py
@@ -41,7 +41,7 @@ def main():
 
     print("size of query is %d" % len(RA))
     print("number of CCDS is %d" % len(ccdtable))
-    attrnames = list(ccdtable.data.dtype.names) + ['NEXP']
+    attrnames = list(ccdtable.data.dtype.names) + ['NEXP','NEXP_G','NEXP_R','NEXP_Z']
     if len(ns.ccdattrs) == 0 or \
        any(attr not in attrnames for attr in ns.ccdattrs):
         raise RuntimeError("ccdattr not found, available ones are %s"
@@ -53,7 +53,10 @@ def main():
 
     r1 = np.zeros_like(RA, dtype=attrdtype)
     r2 = np.zeros_like(RA, dtype=attrdtype)
-    N = np.zeros_like(RA, dtype='f8')
+    N  = np.zeros_like(RA, dtype='f8')
+    NG = np.zeros_like(RA, dtype='f8')
+    NR = np.zeros_like(RA, dtype='f8')
+    NZ = np.zeros_like(RA, dtype='f8')
 
     querytree = points(RA, DEC).tree
     
@@ -64,20 +67,32 @@ def main():
 
         v = ccdtable.data[i]
         for attr in ns.ccdattrs:
-            if attr != 'NEXP':
+            if attr not in ['NEXP','NEXP_G','NEXP_R','NEXP_Z']:
                 np.add.at(r1[attr], j, v[attr])
                 np.add.at(r2[attr], j, v[attr] ** 2)
         np.add.at(N, j, 1)
+        np.add.at(NG, j[v['FILTER'] == bytes('g','utf-8')], 1)
+        np.add.at(NR, j[v['FILTER'] == bytes('r','utf-8')], 1)
+        np.add.at(NZ, j[v['FILTER'] == bytes('z','utf-8')], 1)
 
     ccdtree.root.enum(querytree.root, np.radians(0.2), process)
 
     for attr in ns.ccdattrs:
-        if attr != 'NEXP':
+        if attr not in ['NEXP','NEXP_G','NEXP_R','NEXP_Z']:
             r1[attr] /= N
-        else:
+        if attr == 'NEXP':
             r1[attr] = N
+        if attr == 'NEXP_G':
+            r1[attr] = NG
+        if attr == 'NEXP_R':
+            r1[attr] = NR
+        if attr == 'NEXP_Z':
+            r1[attr] = NZ
+            
+        if ((NG + NR + NZ) != N).sum() != 0:
+            print('Exposures do not add up...')
 
-        print('mean and std of %s from the query is %g %g' % (attr, r1[attr].mean(), r1[attr].std()))
+        print('mean and std of %s from the query is %g %g' % (attr, np.nanmean(r1[attr]), np.nanstd(r1[attr])))
 
     with h5py.File(ns.query, 'r+') as ff:
         for attrname in ns.ccdattrs:
